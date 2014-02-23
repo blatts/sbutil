b28b2c3a2a606ff7a4b39ae0ed2a52cfba5516e1
Sebastian Blatt <sblatt@nospam.tld>
2014-02-23 00:03:14 -0500

    Template for main.cc

--- bin/create_project.py
+++ bin/create_project.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- mode: Python; coding: latin-1 -*-
-# Time-stamp: "2014-02-22 23:46:38 sb"
+# Time-stamp: "2014-02-23 00:02:40 sb"
 
 #  file       create_project.py
 #  copyright  (c) Sebastian Blatt 2014
 
-import os, sys, subprocess
+import os, sys, subprocess, re, os.path
 
 def run_external(cmd):
   print "run_external: \"%s\"" % (cmd)
@@ -16,6 +16,15 @@ def run_external(cmd):
   except OSError:
     print "Failed running external command."
     return None
+
+def copy_replace_template(ifile, ofile, replacements):
+  x = None
+  with open(ifile, 'r') as f:
+    x = f.read()
+  for k, v in replacements.items():
+    x = re.sub(r':::%s:::' % k, v, x)
+  with open(ofile, 'w') as f:
+    f.write(x)
 
 if __name__ == "__main__":
   project = sys.argv[1]
@@ -29,6 +38,18 @@ if __name__ == "__main__":
 
   print "Clone sbutil git repository"
   run_external(['git', 'clone', 'https://github.com/blatts/sbutil.git'])
+
+  # now have access to templates, copy and substitute
+  replacements = {
+    r'project' : project,
+    r'today' : '19990909',
+    r'year' : '2041'
+    }
+
+  copy_replace_template(os.path.join('.','sbutil','templates','main.cc'),
+                        os.path.join('.','%.cc' % project),
+                        replacements)
+
 
 
 # create_project.py ends here
---
+++ templates/main.cc
@@ -0,0 +1,40 @@
+// -*- mode: C++ -*-
+// Time-stamp: "2014-02-23 00:01:08 sb"
+
+/*
+  file       :::project:::.cc
+  copyright  (c) Sebastian Blatt :::year:::
+
+ */
+
+#define PROGRAM_NAME ":::project:::"
+#define PROGRAM_DESCRIPTION "My new program"
+#define PROGRAM_COPYRIGHT "(C) Sebastian Blatt :::year:::"
+#define PROGRAM_VERSION ":::today:::"
+
+#define DEBUG 1
+
+#include <iostream>
+
+#include "CommandLine.hh"
+
+static const char* program_options[] =
+{
+  "Output file", "output", "o", "test.txt",
+  };
+
+int main(int argc, char** argv)
+{
+  CommandLine cl(argc, argv);
+  DWIM_CommandLine(cl,
+                   PROGRAM_NAME,
+                   PROGRAM_DESCRIPTION,
+                   PROGRAM_VERSION,
+                   PROGRAM_COPYRIGHT,
+                   program_options,
+                   sizeof(program_options)/sizeof(char*)/4);
+
+  return 0;
+}
+
+// :::project:::.cc ends here
