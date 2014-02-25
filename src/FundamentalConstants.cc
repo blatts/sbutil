b8dfad2311235f5e28053a5816e3275d5a46e23d
Sebastian Blatt <sblatt@nospam.tld>
2014-02-25 17:34:49 -0500

    More constants.

--- src/FundamentalConstants.cc
+++ src/FundamentalConstants.cc
@@ -1,5 +1,5 @@
 // -*- mode: C++ -*-
-// Time-stamp: "2014-02-25 16:56:41 sb"
+// Time-stamp: "2014-02-25 17:33:01 sb"
 
 /*
   file       FundamentalConstants.cc
@@ -11,15 +11,59 @@
 
 #include <cmath>
 
+#define SQR(x) ((x)*(x))
+
 using namespace FundamentalConstants;
 
-const double pi = 4.0 * atan(1.0);
+// -------------------------------------------------------- Fundamental constants
 
-const double hplanck = 6.62606896e-34;
-const double hbar = FundamentalConstants::hplanck/(2.0 * FundamentalConstants::pi);
-const double kB = 1.3806503207853104e-23;
+// Values obtained from Frink, see
+//
+//   https://futureboy.us/frinkdocs/
+
+
 const double amu = 1.660538782e-27;
-const double bohrradius = 5.29177210791590e-11;
+// could have used integer, but avoid confusion and errors by using double
+const double c = 299792458;
+
+const double electroncharge = 1.602176565e-19;
+
+const double electronmass = 9.10938291e-31;
+
+const double h = 6.62606896e-34;
+
+const double kB = 1.3806503207853104e-23;
+
+// ------------------------------------------------------- Mathematical constants
+
+// Pi to machine precision can be calculated easily as
+//
+//   const double pi = 4.0 * atan(1.0);
+//
+// For compatibility with math.h, use the macro definition from the
+// header here.
+
+const double pi = M_PI;
+
+// ----------------------------------------------------------- Derived quantities
+
+// Derived quantities
+
+const double mu0 = 4.0e-7 * FundamentalConstants::pi;
+
+const double epsilon0 = 1.0 / (FundamentalConstants::mu0) / SQR(FundamentalConstants::c);
+
+const double alpha = 0.25 / (FundamentalConstants::pi * FundamentalConstants::epsilon0) * \
+                     SQR(FundamentalConstants::electroncharge) / \
+                     (FundamentalConstants::hbar * FundamentalConstants::c);
+
+const double hbar = FundamentalConstants::h/(2.0 * FundamentalConstants::pi);
+
+const double bohrradius = FundamentalConstants::hbar / FundamentalConstants::electronmass / \
+                          FundamentalConstants::c / FundamentalConstants::alpha;
+
+
+
 
 
 // FundamentalConstants.cc ends here
--- src/FundamentalConstants.hh
+++ src/FundamentalConstants.hh
@@ -1,9 +1,12 @@
 // -*- mode: C++ -*-
-// Time-stamp: "2014-02-25 16:55:36 sb"
+// Time-stamp: "2014-02-25 17:34:16 sb"
 
 /*
   file       FundamentalConstants.hh
   copyright  (c) Sebastian Blatt 2014
+
+  Fundamental constants in SI units. See FundamentalConstants.cc for
+  details.
 
  */
 
@@ -11,15 +14,28 @@
 #ifndef FUNDAMENTALCONSTANTS_HH__18958542_606F_4D4D_9A94_0F5ADD754002
 #define FUNDAMENTALCONSTANTS_HH__18958542_606F_4D4D_9A94_0F5ADD754002
 
-
 namespace FundamentalConstants
 {
-  extern const double pi;
-  extern const double hplanck;
-  extern const double hbar;
-  extern const double kB;
+  // Fundamental constants
   extern const double amu;
+  extern const double c;
+  extern const double electroncharge;
+  extern const double electronmass;
+  extern const double h;
+  extern const double kB;
+
+
+  // Mathematical constants
+  extern const double pi;
+
+
+  // Derived quantities
+  extern const double alpha;
   extern const double bohrradius;
+  extern const double epsilon0;
+  extern const double hbar;
+  extern const double mu0;
+
 }
 
 #endif // FUNDAMENTALCONSTANTS_HH__18958542_606F_4D4D_9A94_0F5ADD754002
